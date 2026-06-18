5a2af0260846a058596f94cb293d17487a06739e
asd2599 <asd2590@naver.com>
2026-06-18 16:48:55 +0900

    feat: add configuration utility file for environment settings

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const isProd =
   import.meta.env.PROD || window.location.hostname.includes('gamestack.store');
 export const SERVER_URL = isProd
-  ? 'https://eggtalk-backend-production.up.railway.app'
+  ? 'https://eggtalk-backend.fly.dev'
   : 'http://localhost:8000';
 
 export const PRESENT_TABLE_URL =
